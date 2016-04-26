ccbace66b4a38116944862ca0ad7fa19d0275e51
Henry Dashwood <hcndashwood@gmail.com>
2016-04-26 16:49:36 +0100

    Add homework

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,51 @@
+def return_10
+  return 10
+end
+
+def add(num_1, num_2)
+  return num_1 + num_2
+end
+
+def subtract(num_1, num_2)
+  return num_1 - num_2
+end
+
+def multiply(num_1, num_2)
+  return num_1 * num_2
+end
+
+def divide(num_1, num_2)
+  return num_1 / num_2
+end
+
+def length_of_string(string)
+  return string.length
+end
+
+def join_string(string_1, string_2)
+  return string_1 + string_2
+end
+
+def add_string_as_number(num_1, num_2)
+  return num_1.to_i + num_2.to_i
+end
+
+def number_to_full_month_name(num)
+  if num == 1
+    return "January"
+  elsif num == 3
+    return "March"
+  elsif num == 9
+    return "September"
+  end
+end
+
+def number_to_short_month_name(num)
+  if num == 1
+    return "Jan"
+  elsif num == 3
+    return "Mar"
+  elsif num == 9
+    return "Sep"
+  end
+end
